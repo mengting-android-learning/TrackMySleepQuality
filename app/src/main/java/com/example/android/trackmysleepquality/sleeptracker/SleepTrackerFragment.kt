551eb7a1dbca9a928dd47caf1fadb18bbe5a2193
Rachel-oc <mengting.xie@thoughtworks.com>
2023-02-24 15:22:06 +0800

    [feat]: use recycler view

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
@@ -59,8 +60,18 @@ class SleepTrackerFragment : Fragment() {
                 this,
                 viewModelFactory
             ).get(SleepTrackerViewModel::class.java)
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter = adapter
         binding.setLifecycleOwner(this)
         binding.sleepTrackerViewModel = sleepTrackerViewModel
+        sleepTrackerViewModel.nights.observe(
+            viewLifecycleOwner,
+            Observer {
+                it?.let {
+                    adapter.data = it
+                }
+            }
+        )
         return binding.root
     }
 }
